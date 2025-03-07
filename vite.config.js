ee675c15c5ad47d8a393f3d020748daac1de6f05
Code-bySalman <usmanis@student.iul.ac.in>
2025-03-07 21:35:26 +0530

    akjshkd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-   base: '/'
-
-})
+  base: './', // Change from '/' to './'
+  build: {
+    assetsInlineLimit: 0, // Ensures all assets are properly resolved
+  },
+});
